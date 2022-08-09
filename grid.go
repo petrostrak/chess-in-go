4826c2e5e125f0960d3af7ac69e7e4eb77d3d3e0
petrostrak <pit.trak@gmail.com>
2022-08-09 17:56:08 +0300

    Correct math and implement random play

--- grid.go
+++ grid.go
@@ -12,17 +12,17 @@ import (
 func createGrid(b *chess.Board) *fyne.Container {
 	grid := container.NewGridWithColumns(8)
 
-	for y := 0; y < 8; y++ {
+	for y := 7; y >= 0; y-- {
 		for x := 0; x < 8; x++ {
-			bg := canvas.NewRectangle(color.Gray{0x30})
+			bg := canvas.NewRectangle(color.Gray{0xE0})
 
 			// alternate color to achieve black n white chess pattern
 			if x%2 == y%2 {
-				bg.FillColor = color.Gray{0xE0}
+				bg.FillColor = color.Gray{0x30}
 			}
 
 			// revert fyne x and y axis to match chess's library
-			piece := b.Piece(chess.Square(x + (7-y)*8))
+			piece := b.Piece(chess.Square(x + y*8))
 
 			img := canvas.NewImageFromResource(resourceForPiece(piece))
 			// image maintains its aspect ratio while in the canvas
@@ -34,4 +34,26 @@ func createGrid(b *chess.Board) *fyne.Container {
 	}
 
 	return grid
+}
+
+func move(m *chess.Move, game *chess.Game, grid *fyne.Container) {
+	game.Move(m)
+	refreshGrid(grid, game.Position().Board())
+}
+
+func refreshGrid(grid *fyne.Container, b *chess.Board) {
+	y, x := 7, 0
+	for _, cell := range grid.Objects {
+		piece := b.Piece(chess.Square(x + y*8))
+
+		img := cell.(*fyne.Container).Objects[1].(*canvas.Image)
+		img.Resource = resourceForPiece(piece)
+		img.Refresh()
+
+		x++
+		if x == 8 {
+			x = 0
+			y--
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"github.com/notnil/chess"
@@ -14,5 +17,19 @@ func main() {
 	grid := createGrid(game.Position().Board())
 	w.SetContent(grid)
 	w.Resize(fyne.NewSize(480, 480))
+
+	go func() {
+		rand.Seed(time.Now().Unix())
+
+		for game.Outcome() == chess.NoOutcome {
+			time.Sleep(time.Millisecond * 500)
+			valid := game.ValidMoves()
+			m := valid[rand.Intn(len(valid))]
+
+			move(m, game, grid)
+		}
+
+	}()
+
 	w.ShowAndRun()
 }
